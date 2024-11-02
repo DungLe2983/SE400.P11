a7a9241a104fee2a3ecb89102c017d84af57b57b
ThanhDong00 <thanhdonganmy@gmail.com>
2024-11-02 15:23:22 +0700

    fix home page

--- project/home/src/HomePage.jsx
+++ project/home/src/HomePage.jsx
@@ -1,48 +1,49 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import OutStandingProduct from './components/OutStandingProduct';
-import WhyChooseUs from './components/WhyChooseUs';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import OutStandingProduct from "./components/OutStandingProduct";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 const HomePage = () => {
-    return (
-        <div className='min-h-screen container mx-auto  font-sans relative flex'>
-            {/* Gradient Background từ header xuống */}
-            <div className='absolute inset-y-0 right-0 h-screen w-1/3 bg-gradient-to-b from-[#AB238F] to-[#FF7770] opacity-30 z-0'></div>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className=" relative">
+        <div className="absolute inset-y-0 right-0 w-1/3 bg-gradient-to-b from-[#AB238F] to-[#FF7770] opacity-30 -z-10"></div>
 
-            <div className='relative z-10 w-full'>
-                {/* Header */}
-                <header >
-                    <Header />
-                </header>
-
-                {/* Main Content */}
-                <main className='flex px-8 mt-20'>
-                    <OutStandingProduct />
-                </main>
-
-                {/* Stats Section */}
-                <section className='w-full mt-12 bg-gray-900 text-white py-8 px-6'>
-                    <div className='flex justify-around text-center'>
-                        <div>
-                            <h3 className='text-4xl font-bold'>500+</h3>
-                            <p className='text-gray-400'>Amazing products</p>
-                        </div>
-                        <div>
-                            <h3 className='text-4xl font-bold'>40k+</h3>
-                            <p className='text-gray-400'>Orders Completed</p>
-                        </div>
-                        <div>
-                            <h3 className='text-4xl font-bold'>32k+</h3>
-                            <p className='text-gray-400'>Happy Customers</p>
-                        </div>
-                    </div>
-                </section>
-
-                <WhyChooseUs />
-            </div>
+        <div className="z-50">
+          <Header />
         </div>
-    );
+
+        {/* Hero Section */}
+        <div className="flex-1">
+          <div className="container mx-auto px-4 py-20">
+            <OutStandingProduct />
+          </div>
+        </div>
+      </div>
+      {/* Stats Section */}
+      <div className="bg-zinc-950 text-white py-12">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
+            <div className="space-y-2">
+              <h3 className="text-4xl font-bold">500+</h3>
+              <p className="text-zinc-400">Amazing products</p>
+            </div>
+            <div className="space-y-2">
+              <h3 className="text-4xl font-bold">40k+</h3>
+              <p className="text-zinc-400">Orders Completed</p>
+            </div>
+            <div className="space-y-2">
+              <h3 className="text-4xl font-bold">32k+</h3>
+              <p className="text-zinc-400">Happy Customers</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <WhyChooseUs />
+    </div>
+  );
 };
 
 export default HomePage;
--- project/home/src/components/OutStandingProduct.jsx
+++ project/home/src/components/OutStandingProduct.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
-import OTProImg from '../assets/OTProImg.png'
+import React from "react";
+import OTProImg from "../assets/OTProImg.png";
 
 const OutStandingProduct = () => {
   return (
-      <div className='flex items-center'>
-          <section className='w-1/2 p-8'>
-              <h3 className='text-pink-500 font-semibold text-sm uppercase mb-6'>
-                  Road Racing Shoes
-              </h3>
-              <h1 className='text-6xl font-extrabold text-gray-900 leading-tight mb-4'>
-                  Nike ZoomX <br /> Streakfly
-              </h1>
-              <p className='text-gray-400 text-base mt-8 mb-14'>
-                  Our lightest racing shoe, the Nike ZoomX Streakfly is all
-                  about the speed you need to take on the competition in a mile,
-                  5K or 10K race.
-              </p>
+    <div className="flex items-center pt-16">
+      <section className="w-1/2 py-8">
+        <h3 className="text-pink-500 font-semibold text-sm uppercase mb-6">
+          Road Racing Shoes
+        </h3>
+        <h1 className="text-6xl font-extrabold text-gray-900 leading-tight mb-4">
+          Nike ZoomX <br /> Streakfly
+        </h1>
+        <p className="text-gray-400 text-base mt-8 mb-14">
+          Our lightest racing shoe, the Nike ZoomX Streakfly is all about the
+          speed you need to take on the competition in a mile, 5K or 10K race.
+        </p>
+      </section>
 
-          </section>
-
-          {/* Product Image */}
-          <section className='w-1/2 relative flex justify-center items-center'>
-              <img
-                  src={OTProImg}
-                  alt='Nike ZoomX Streakfly'
-                  className='w-[600] h-[400] object-cover relative z-10'
-              />
-          </section>
-      </div>
+      {/* Product Image */}
+      <section className="w-1/2 flex justify-center items-center ">
+        <img
+          src={OTProImg}
+          alt="Nike ZoomX Streakfly"
+          className="w-[600] h-[400] object-cover "
+        />
+      </section>
+    </div>
   );
-}
+};
 
-export default OutStandingProduct
+export default OutStandingProduct;
